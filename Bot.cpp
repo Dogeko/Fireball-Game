64ad93f179093fc05ea95b2e8443b80d496cb7b4
unknown <dogeko852@gmail.com>
2020-05-01 23:56:32 -0700

    haha formating go brrr

--- Bot.cpp
+++ Bot.cpp
@@ -15,37 +15,46 @@ void Bot::setChoice(int c)
   choice = c;
 }
 
-void Bot::pick() {
+void Bot::pick()
+{
     choice = (rand() % 4) + 1;
-    switch (choice) {
-        case 1:
-            cout << "your opponent charged." << endl;
-            setChoice(1);
-            charges++; //idk if this works in cpp haha
-            cout<<"charge is "<<charges<<endl;
-            break;
-        case 2:
-            cout << "your opponent shielded." << endl;
-            setChoice(2);
-            cout<<"charge is "<<charges<<endl;
-            break;
-            //somethingsomething
-        case 3:
-            if (charges > 1) {
-                pick();
-            } else {
-                cout << "your opponent wanted to light you on fire" << endl;
-                charges--;
-                cout<<"charge is "<<charges<<endl;
-                //make other play go boom
-            }
-            break;
-        case 4:
-            if (charges < 3)
-                pick();
-            else
-                charges = charges - 3;
-            //make other player go big boom.
-            break;
+    switch (choice)
+    {
+    case 1:
+        cout << "your opponent charged." << endl;
+        setChoice(1);
+        charges++; //idk if this works in cpp haha
+        cout << "charge is " << charges << endl;
+        break;
+    case 2:
+        cout << "your opponent shielded." << endl;
+        setChoice(2);
+        cout << "charge is " << charges << endl;
+        break;
+        //somethingsomething
+    case 3:
+        if (charges < 1)
+        {
+            pick();
+        }
+        else
+        {
+            cout << "your opponent wanted to light you on fire" << endl;
+            charges--;
+            cout << "charge is " << charges << endl;
+            //make other play go boom
+        }
+        break;
+    case 4:
+        if (charges < 3)
+        {
+            pick();
+        }
+        else
+        {
+            charges = charges - 3;
+        }
+        //make other player go big boom.
+        break;
     }
 }
--- Game.cpp
+++ Game.cpp
@@ -3,53 +3,64 @@
 
 void Game::play()
 {
-	// cout << "Press 1 if you want to play against a bot"
-  Human p1;
-  Human p2;
-  bool check = false;
-  while(!check){
-    p1.Pick();
-    cout<<"player 1 choose" << p1.getChoice()<<endl;
-    p2.Pick();
-      cout<<"player 2 choose" << p2.getChoice()<<endl;
-    check = logic(p1.getChoice(),p2.getChoice());
-  }
+    // cout << "Press 1 if you want to play against a bot"
+    Human p1;
+    Human p2;
+    bool check = false;
+    while (!check)
+    {
+        p1.Pick();
+        cout<< "player 1 choose" << p1.getChoice() << endl;
+        p2.Pick();
+        cout<< "player 2 choose" << p2.getChoice() << endl;
+        check = logic(p1.getChoice(), p2.getChoice());
+    }
 }
 
 bool Game::logic(int p1Choice, int p2Choice)
 {
-  if(p1Choice == 3 && p2Choice == 1){
-    cout << "Player one fried Player 2"<<endl;
-    return true;
-  }
-  else if(p1Choice == 1 && p2Choice == 3){
-    cout << "Player two fried Player one" <<endl;
-    return true;
-  }
-  else if(p1Choice == 4 && p2Choice != 4){
-    cout << "Player one is Bruce Lee" << endl;
-    return true;
-  }
-  else if(p1Choice != 4 && p2Choice == 4){
-    cout << "Player two is Bruce Lee" << endl;
-    return true;
-  }
-  else{
+    if (p1Choice == 3 && p2Choice == 1)
+    {
+        cout << "Player one fried Player 2" << endl;
+        return true;
+    }
+    else if (p1Choice == 1 && p2Choice == 3)
+    {
+        cout << "Player two fried Player one" << endl;
+        return true;
+    }
+    else if (p1Choice == 4 && p2Choice != 4)
+    {
+        cout << "Player one is Bruce Lee" << endl;
+        return true;
+    }
+    else if (p1Choice != 4 && p2Choice == 4)
+    {
+        cout << "Player two is Bruce Lee" << endl;
+        return true;
+    }
+    else{
 
-    if(p1Choice == 4 && p2Choice ==4){
-          cout<<"start RPS!!!"<<endl;
-      }
-    else if(p1Choice == p2Choice){
-        cout<<"noting happends"<<endl;
+        if(p1Choice == 4 && p2Choice ==4)
+        {
+                cout << "start RPS!!!" << endl;
+        }
+        else if (p1Choice == p2Choice)
+        {
+            cout << "noting happends" << endl;
+        }
+        else if ((p1Choice == 1|2) && (p1Choice == 1|2))
+        {
+            cout << "noting happends" << endl;
+        }
+        else if (p1Choice == 2 && p2Choice == 3)
+        {
+            cout << "p1 blocked p2's fireball" << endl;
+        }
+        else if (p1Choice == 3 && p2Choice == 2)
+        {
+            cout << "p2 blocked p1's fireball" << endl;
+        }
+        return false;
     }
-    else if((p1Choice == 1|2)&&(p1Choice == 1|2)){
-        cout<<"noting happends"<<endl;
-    }
-    else if(p1Choice == 2 && p2Choice == 3){
-        cout<<"p1 blocked p2's fireball"<<endl;
-    }else if(p1Choice == 3 && p2Choice == 2){
-        cout<<"p2 blocked p1's fireball"<<endl;
-    }
-    return false;
-  }
 }
