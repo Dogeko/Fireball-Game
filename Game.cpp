b20912b7670b62bc7d91776fb79c3319e286b19b
unknown <dogeko852@gmail.com>
2020-05-04 20:53:15 -0700

    P R O J E C T B A S I C A L L Y D O N E

--- Game.cpp
+++ Game.cpp
@@ -4,6 +4,9 @@ void Game::play()
 {
     // cout << "Press 1 if you want to play against a bot"
     cout << "Welcome to Fireball" << endl;
+    cout << endl << "Kill your opponent with fireball or karate chop." << endl;
+    cout << "Fireball can be blocked. Karate chop beats all." << endl;
+    cout << "Fireball = 1 charge    Karate chop = 3 charges" << endl << endl;
     cout << "Press 1 if you want to play against a human" << endl;
     cout << "Press 2 if you want to play against a bot." << endl;
     int opponent;
@@ -32,7 +35,7 @@ void Game::play()
       score.incrementP2();
     }
     char answer;
-    cout << "Do you want to play again? Press Y to play again. ANY OTHER KEY WILL END THE GAME LMAO" << endl;
+    cout << "Do you want to play again? Press Y to play again. ANY OTHER KEY WILL END THE GAME" << endl;
     cin >> answer;
     if(tolower(answer) == 'y')
     {
@@ -40,7 +43,7 @@ void Game::play()
     }
     else
     {
-      cout << "GAME OVER LOSER" << endl;
+      cout << "GAME OVER" << endl;
       score.displayResults();
     }
 
@@ -51,50 +54,49 @@ Player* Game::logic(Player* p1, Player* p2)
 {
     if (p1->getChoice() == 3 && p2->getChoice() == 1)
     {
-        cout << "Player one fried Player 2" << endl;
+        cout << "Player 1 burns Player 2" << endl;
         return p1;
     }
     else if (p1->getChoice() == 1 && p2->getChoice() == 3)
     {
-        cout << "Player two fried Player one" << endl;
+        cout << "Player 2 burns Player 1" << endl;
         return p2;
     }
     else if (p1->getChoice() == 4 && p2->getChoice() != 4)
     {
-        cout << "Player one is Bruce Lee" << endl;
+        cout << "Player 1 slices Player 2 in half" << endl;
         return p1;
     }
     else if (p1->getChoice() != 4 && p2->getChoice() == 4)
     {
-        cout << "Player two is Bruce Lee" << endl;
+        cout << "Player 2 slices Player 1 in half" << endl;
         return p2;
     }
     else
     {
-
         if(p1->getChoice() == 4 && p2->getChoice() == 4)
         {
-                cout << "start RPS!!!" << endl;
+            cout << "Both karate chops collide and bounce off each other. Nothing happens." << endl;
         }
         else if (p1->getChoice() == p2->getChoice())
         {
-            cout << "nothing happends" << endl;
+            cout << "Nothing happens" << endl;
         }
         else if ((p1->getChoice() == 1|2) && (p2->getChoice() == 1|2))
         {
-            cout << "nothing happends" << endl;
+            cout << "Nothing happens" << endl;
         }
         else if (p1->getChoice() == 2 && p2->getChoice() == 3)
         {
-            cout << "p1 blocked p2's fireball" << endl;
+            cout << "Player 1 blocks Player 2's fireball" << endl;
         }
         else if (p1->getChoice() == 3 && p2->getChoice() == 2)
         {
-            cout << "p2 blocked p1's fireball" << endl;
+            cout << "Player 2 blocks Player 1's fireball" << endl;
         }
         else if(p1->getChoice() == 3 && p2->getChoice() == 3)
         {
-            cout << "Your fireballs made a fireball. Nothing happened" << endl;
+            cout << "Both fireballs collide and dissipates. Nothing happens" << endl;
         }
         return NULL;
     }
